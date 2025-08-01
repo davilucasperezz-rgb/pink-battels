68e3b268fb96165c73f1e69664870a12f26e81af
davilucasperezz-rgb <davilucasperezz@gmail.com>
2025-08-01 12:38:30 -0300

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,26 @@
+ 
+
+
+> Open this page at [https://davilucasperezz-rgb.github.io/pink-battels/](https://davilucasperezz-rgb.github.io/pink-battels/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/davilucasperezz-rgb/pink-battels** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/davilucasperezz-rgb/pink-battels** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ images.g.ts
@@ -0,0 +1,64 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "noob":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . 5 5 5 5 5 5 5 . . . . 
+. . . . . 5 5 f 5 f 5 5 . . . . 
+. . . . . 5 5 5 5 5 5 5 . . . . 
+. . . . . 5 5 5 5 5 5 5 . . . . 
+. . . . . 5 5 f f f 5 5 . . . . 
+. . . . . 5 5 5 5 5 5 5 . . . . 
+. . . 5 5 8 8 8 8 8 8 8 5 5 . . 
+. . . 5 5 8 8 8 8 8 8 8 5 5 . . 
+. . . 5 5 8 8 8 8 8 8 8 5 5 . . 
+. . . . . 8 8 8 8 8 8 8 . . . . 
+. . . . . . 7 7 . 7 7 . . . . . 
+. . . . . . 7 7 . 7 7 . . . . . 
+`;
+            case "image2":
+            case "bola branca":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 1 1 1 1 1 1 1 1 1 . . . . 
+. . . 1 1 1 1 1 1 1 1 1 . . . . 
+. . . 1 1 1 f 1 f 1 1 1 . . . . 
+. . . 1 1 1 1 1 1 1 1 1 . . . . 
+. . . 1 1 1 1 1 1 1 1 1 . . . . 
+. . . 1 1 f 1 1 1 f 1 1 . . . . 
+. . . 1 1 f 1 1 1 f 1 1 . . . . 
+. . . 1 1 f f f f f 1 1 . . . . 
+. . . 1 1 1 1 1 1 1 1 1 . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "song1":
+            case "POWER UP":return hex`0078000408020100001c00010a006400f401640000040000000000000000000000000005000004120008000c0001270c0010000127100014000127`;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
